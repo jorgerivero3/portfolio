0fa3da70556e73d9a80d7698b96e595628394ddf
jorgerivero3 <20407259+jorgerivero3@users.noreply.github.com>
2025-01-02 15:56:18 -0600

    Added resume route and edited portfolio details

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/resume', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'resume.pdf'));
+})
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`)
 });
