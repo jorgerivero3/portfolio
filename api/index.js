5a3ee20147838bab5e6a042bbf3b7fc664949e2e
jorgerivero3 <20407259+jorgerivero3@users.noreply.github.com>
2025-01-02 21:51:30 -0600

    Changed to resume.pdf

--- api/index.js
+++ api/index.js
@@ -1,31 +1,19 @@
-const express = require('express');
-const path = require ('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const express = require('express');
+// const path = require ('path');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-//compression does not compress jpeg files
-// app.use(compression({
-//     filter: (req, res) => {
-//         const contentType = res.getHeader('Content-Type');
-//         if (contentType && contentType.includes('image/jpeg')){
-//             return false;
-//         }
+// app.use(express.static(path.join(__dirname, 'public')));
 
-//         return compression.filter(req, res);
-//     }
-// }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 // app.get('/resume', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'resume.pdf'));
 // })
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`)
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}/`)
+// });
 
