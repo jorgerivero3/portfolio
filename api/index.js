5ebc34675a3f68f8c80889d1b11db6a306f3372c
jorgerivero3 <20407259+jorgerivero3@users.noreply.github.com>
2025-01-02 21:58:12 -0600

    Changed index

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
-// const express = require('express');
-// const path = require ('path');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const express = require('express');
+const path = require ('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
+});
 
-// app.get('/resume', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'resume.pdf'));
-// })
+app.get('/resume', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'resume.pdf'));
+})
 
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}/`)
-// });
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`)
+});
 
